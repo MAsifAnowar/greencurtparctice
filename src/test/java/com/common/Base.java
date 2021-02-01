9aa1b1c9f1801ec600fa211bde87e6c1068a48f1
asif39198 <asif39198@192.168.0.3>
2021-01-31 19:54:58 -0500

    hello

---
+++ src/test/java/com/common/Base.java
@@ -0,0 +1,29 @@
+package com.common;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+
+public class Base {
+
+	public WebDriver driver;
+	
+public WebDriver getdriver() {
+	
+	WebDriverManager.chromedriver().setup();
+	driver = new ChromeDriver();
+	
+	
+	driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	return driver;
+	
+	
+}
+}
